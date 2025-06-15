74a3afb9898899e45bfa68084a55e29b2456f40d
yamaguchi-sd <tkt1933thr@gmail.com>
2025-06-15 17:16:58 +0900

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -13,16 +13,16 @@ import Array7 from '../views/workbook/Array/7.vue'
 import Array8 from '../views/workbook/Array/8.vue'
 import Array9 from '../views/workbook/Array/9.vue'
 import Array10 from '../views/workbook/Array/10.vue'
-import Algorithm1 from '../views/workbook/Algorithm/1.vue'
-import Algorithm2 from '../views/workbook/Algorithm/2.vue'
-import Algorithm3 from '../views/workbook/Algorithm/3.vue'
-import Algorithm4 from '../views/workbook/Algorithm/4.vue'
-import Algorithm5 from '../views/workbook/Algorithm/5.vue'
-import Algorithm6 from '../views/workbook/Algorithm/6.vue'
-import Algorithm7 from '../views/workbook/Algorithm/7.vue'
-import Algorithm8 from '../views/workbook/Algorithm/8.vue'
-import Algorithm9 from '../views/workbook/Algorithm/9.vue'
-import Algorithm10 from '../views/workbook/Algorithm/10.vue'
+import Grammar1 from '../views/workbook/Grammar/1.vue'
+import Grammar2 from '../views/workbook/Grammar/2.vue'
+import Grammar3 from '../views/workbook/Grammar/3.vue'
+import Grammar4 from '../views/workbook/Grammar/4.vue'
+import Grammar5 from '../views/workbook/Grammar/5.vue'
+import Grammar6 from '../views/workbook/Grammar/6.vue'
+import Grammar7 from '../views/workbook/Grammar/7.vue'
+import Grammar8 from '../views/workbook/Grammar/8.vue'
+import Grammar9 from '../views/workbook/Grammar/9.vue'
+import Grammar10 from '../views/workbook/Grammar/10.vue'
 import List1 from '../views/workbook/List/1.vue'
 import List2 from '../views/workbook/List/2.vue'
 import List3 from '../views/workbook/List/3.vue'
@@ -33,16 +33,6 @@ import List7 from '../views/workbook/List/7.vue'
 import List8 from '../views/workbook/List/8.vue'
 import List9 from '../views/workbook/List/9.vue'
 import List10 from '../views/workbook/List/10.vue'
-import AlgorithmComplexity1 from '../views/workbook/AlgorithmComplexity/1.vue'
-import AlgorithmComplexity2 from '../views/workbook/AlgorithmComplexity/2.vue'
-import AlgorithmComplexity3 from '../views/workbook/AlgorithmComplexity/3.vue'
-import AlgorithmComplexity4 from '../views/workbook/AlgorithmComplexity/4.vue'
-import AlgorithmComplexity5 from '../views/workbook/AlgorithmComplexity/5.vue'
-import AlgorithmComplexity6 from '../views/workbook/AlgorithmComplexity/6.vue'
-import AlgorithmComplexity7 from '../views/workbook/AlgorithmComplexity/7.vue'
-import AlgorithmComplexity8 from '../views/workbook/AlgorithmComplexity/8.vue'
-import AlgorithmComplexity9 from '../views/workbook/AlgorithmComplexity/9.vue'
-import AlgorithmComplexity10 from '../views/workbook/AlgorithmComplexity/10.vue'
 import QueueStack1 from '../views/workbook/QueueStack/1.vue'
 import QueueStack2 from '../views/workbook/QueueStack/2.vue'
 import QueueStack3 from '../views/workbook/QueueStack/3.vue'
@@ -83,16 +73,6 @@ import DataSearching7 from '../views/workbook/DataSearching/7.vue'
 import DataSearching8 from '../views/workbook/DataSearching/8.vue'
 import DataSearching9 from '../views/workbook/DataSearching/9.vue'
 import DataSearching10 from '../views/workbook/DataSearching/10.vue'
-import ProgramAttributes1 from '../views/workbook/ProgramAttributes/1.vue'
-import ProgramAttributes2 from '../views/workbook/ProgramAttributes/2.vue'
-import ProgramAttributes3 from '../views/workbook/ProgramAttributes/3.vue'
-import ProgramAttributes4 from '../views/workbook/ProgramAttributes/4.vue'
-import ProgramAttributes5 from '../views/workbook/ProgramAttributes/5.vue'
-import ProgramAttributes6 from '../views/workbook/ProgramAttributes/6.vue'
-import ProgramAttributes7 from '../views/workbook/ProgramAttributes/7.vue'
-import ProgramAttributes8 from '../views/workbook/ProgramAttributes/8.vue'
-import ProgramAttributes9 from '../views/workbook/ProgramAttributes/9.vue'
-import ProgramAttributes10 from '../views/workbook/ProgramAttributes/10.vue'
 
 const routes = [
   { path: '/', component: Home },
@@ -101,8 +81,8 @@ const routes = [
   { path: '/contact', component: Contact },
 
   {
-    path: '/reference/algorithm',
-    component: () => import('../views/reference/Algorithm.vue'),
+    path: '/reference/grammar',
+    component: () => import('../views/reference/Grammar.vue'),
   },
   {
     path: '/reference/array',
@@ -128,14 +108,7 @@ const routes = [
     path: '/reference/search',
     component: () => import('../views/reference/DataSearching.vue'),
   },
-  {
-    path: '/reference/complexity',
-    component: () => import('../views/reference/AlgorithmComplexity.vue'),
-  },
-  {
-    path: '/reference/attribute',
-    component: () => import('../views/reference/ProgramAttributes.vue'),
-  },
+
   { path: '/workbook/array1', component: Array1 },
   { path: '/workbook/array2', component: Array2 },
   { path: '/workbook/array3', component: Array3 },
@@ -146,16 +119,16 @@ const routes = [
   { path: '/workbook/array8', component: Array8 },
   { path: '/workbook/array9', component: Array9 },
   { path: '/workbook/array10', component: Array10 },
-  { path: '/workbook/algorithm1', component: Algorithm1 },
-  { path: '/workbook/algorithm2', component: Algorithm2 },
-  { path: '/workbook/algorithm3', component: Algorithm3 },
-  { path: '/workbook/algorithm4', component: Algorithm4 },
-  { path: '/workbook/algorithm5', component: Algorithm5 },
-  { path: '/workbook/algorithm6', component: Algorithm6 },
-  { path: '/workbook/algorithm7', component: Algorithm7 },
-  { path: '/workbook/algorithm8', component: Algorithm8 },
-  { path: '/workbook/algorithm9', component: Algorithm9 },
-  { path: '/workbook/algorithm10', component: Algorithm10 },
+  { path: '/workbook/grammar1', component: Grammar1 },
+  { path: '/workbook/grammar2', component: Grammar2 },
+  { path: '/workbook/grammar3', component: Grammar3 },
+  { path: '/workbook/grammar4', component: Grammar4 },
+  { path: '/workbook/grammar5', component: Grammar5 },
+  { path: '/workbook/grammar6', component: Grammar6 },
+  { path: '/workbook/grammar7', component: Grammar7 },
+  { path: '/workbook/grammar8', component: Grammar8 },
+  { path: '/workbook/grammar9', component: Grammar9 },
+  { path: '/workbook/grammar10', component: Grammar10 },
   { path: '/workbook/list1', component: List1 },
   { path: '/workbook/list2', component: List2 },
   { path: '/workbook/list3', component: List3 },
@@ -166,16 +139,7 @@ const routes = [
   { path: '/workbook/list8', component: List8 },
   { path: '/workbook/list9', component: List9 },
   { path: '/workbook/list10', component: List10 },
-  { path: '/workbook/complexity1', component: AlgorithmComplexity1 },
-  { path: '/workbook/complexity2', component: AlgorithmComplexity2 },
-  { path: '/workbook/complexity3', component: AlgorithmComplexity3 },
-  { path: '/workbook/complexity4', component: AlgorithmComplexity4 },
-  { path: '/workbook/complexity5', component: AlgorithmComplexity5 },
-  { path: '/workbook/complexity6', component: AlgorithmComplexity6 },
-  { path: '/workbook/complexity7', component: AlgorithmComplexity7 },
-  { path: '/workbook/complexity8', component: AlgorithmComplexity8 },
-  { path: '/workbook/complexity9', component: AlgorithmComplexity9 },
-  { path: '/workbook/complexity10', component: AlgorithmComplexity10 },
+
   { path: '/workbook/queue-stack1', component: QueueStack1 },
   { path: '/workbook/queue-stack2', component: QueueStack2 },
   { path: '/workbook/queue-stack3', component: QueueStack3 },
@@ -216,16 +180,6 @@ const routes = [
   { path: '/workbook/data-searching8', component: DataSearching8 },
   { path: '/workbook/data-searching9', component: DataSearching9 },
   { path: '/workbook/data-searching10', component: DataSearching10 },
-  { path: '/workbook/program-attributes1', component: ProgramAttributes1 },
-  { path: '/workbook/program-attributes2', component: ProgramAttributes2 },
-  { path: '/workbook/program-attributes3', component: ProgramAttributes3 },
-  { path: '/workbook/program-attributes4', component: ProgramAttributes4 },
-  { path: '/workbook/program-attributes5', component: ProgramAttributes5 },
-  { path: '/workbook/program-attributes6', component: ProgramAttributes6 },
-  { path: '/workbook/program-attributes7', component: ProgramAttributes7 },
-  { path: '/workbook/program-attributes8', component: ProgramAttributes8 },
-  { path: '/workbook/program-attributes9', component: ProgramAttributes9 },
-  { path: '/workbook/program-attributes10', component: ProgramAttributes10 },
 ]
 
 const router = createRouter({
