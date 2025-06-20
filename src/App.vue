5e15e4b03083d8cc1ec0d3d8a02c72b3ea2d5e7d
yamaguchi-sd <tkt1933thr@gmail.com>
2025-06-20 16:29:47 +0900

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NavBar />
-    <router-view :status-map="statusMap" @update-status="updateStatus" />
+    <router-view :statusMap="statusMap" @update-status="updateStatus" />
   </div>
 </template>
 
@@ -9,29 +9,36 @@
 import NavBar from './components/NavBar.vue'
 
 export default {
-  components: {
-    NavBar,
-  },
+  components: { NavBar },
   data() {
     return {
-      statusMap: {
-        1: 'unanswered',
-        2: 'unanswered',
-        3: 'unanswered',
-        4: 'unanswered',
-        5: 'unanswered',
-        6: 'unanswered',
-        7: 'unanswered',
-        8: 'unanswered',
-        9: 'unanswered',
-        10: 'unanswered',
-      },
+      statusMap: this.initStatusMap(),
     }
   },
   methods: {
+    initStatusMap() {
+      const map = {}
+      for (let i = 1; i <= 10; i++) {
+        map[i] = 'unanswered'
+      }
+      return map
+    },
     updateStatus({ number, status }) {
       this.statusMap[number] = status
     },
+    watchRouteReset(to) {
+      // ✅ 正規表現で /workbook/array3, /workbook/algorithm9 などにマッチ
+      const isProblemPage = /^\/workbook\/[a-zA-Z]+\d+$/.test(to.path)
+      if (!isProblemPage) {
+        this.statusMap = this.initStatusMap()
+        sessionStorage.setItem('showPopup', 'true') // ポップアップ初期表示に戻す（任意）
+      }
+    },
+  },
+  mounted() {
+    this.$router.afterEach((to) => {
+      this.watchRouteReset(to)
+    })
   },
 }
 </script>
